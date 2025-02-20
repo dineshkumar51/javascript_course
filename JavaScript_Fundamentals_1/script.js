3d7e8086e5175fd8e2c9b791ec35c1513733cdfd
dineshkumar1551 <dineshkumar1551@gmail.com>
2025-02-20 23:32:20 +0530

    9

--- JavaScript_Fundamentals_1/script.js
+++ JavaScript_Fundamentals_1/script.js
@@ -3,3 +3,11 @@ let js = 'amazing';
 if (js === 'amazing') alert('Javascript is FUN!');
 
 console.log(10 + 20 - 40);
+
+let country = 'India';
+let continent = 'Asia';
+let population = 1438100000;
+console.log(country);
+console.log(continent);
+console.log(population);
+
