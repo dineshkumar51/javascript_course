799d1003ad96b581aa305ae0776c3bd0b27cc1c0
dineshkumar1551 <dineshkumar1551@gmail.com>
2025-02-23 10:47:46 +0530

    30

--- JavaScript_Fundamentals_1/script.js
+++ JavaScript_Fundamentals_1/script.js
@@ -226,4 +226,13 @@ switch (day) {
 
 
 
+const bill = 275;
+
+const tip = (bill >= 50 && bill <= 300) ? (bill * 0.15) : (bill * 0.2);
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+
+
+
 
